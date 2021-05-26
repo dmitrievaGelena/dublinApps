5b9540f990b4e0e16c300620a6ede6d2ce82c2d3
Gelena Dmitrieva <dmitrievaGelenaOneTwo@gmail.com>
2021-05-26 14:58:32 +0300

    add header

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
 $(function() {
-
+    $('.header__inner-slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        prevArrow: '<button class="slick-arrow next-arrow"><img src="img/header/next.png" alt=""></button>',
+        nextArrow: '<button class="slick-arrow prev-arrow"><img src="img/header/prev.png" alt=""></button>',
+      });
+          
 });
