10f4e7e9ff7ff0348e1a33f2382b5651fd05d095
Gelena Dmitrieva <dmitrievaGelenaOneTwo@gmail.com>
2021-06-05 22:48:56 +0300

    add popup window

--- gulpfile.js
+++ gulpfile.js
@@ -34,6 +34,7 @@ gulp.task('css', function() {
     return gulp.src([
             'node_modules/normalize.css/normalize.css',
             'node_modules/slick-carousel/slick/slick.css',
+            'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css',
         ])
         .pipe(concat('_libs.scss'))
         .pipe(gulp.dest('app/scss'))
@@ -61,6 +62,7 @@ gulp.task('script', function() {
 gulp.task('js', function() {
     return gulp.src([
             'node_modules/slick-carousel/slick/slick.js',
+            'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
         ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
